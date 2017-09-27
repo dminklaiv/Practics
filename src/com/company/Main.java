c2c4f50015d3398706994a5d09ba1c6bbef54857
Джарвис <demian1995king@gmail.com>
2017-09-28 00:59:53 +0300

    Task 3-6

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -4,12 +4,26 @@ public class Main {
 
     public static void main(String[] args) {
         print(args);
+        sort(args);
+        print(args);
     }
 
     public static void print(String[] args) {
-        for(String str: args)
-        {
+        for(String str: args) {
             System.out.println(str);
+        }
+    }
+
+    public static void sort(String[] args)
+    {
+        for(int i = 0; i < args.length-1; i++) {
+            for(int j = i+1; j < args.length; j++){
+                if(args[j].compareTo(args[i]) < 0){
+                    String swap = args[i];
+                    args[i] = args[j];
+                    args[j] = swap;
+                }
+            }
         }
     }
 }
